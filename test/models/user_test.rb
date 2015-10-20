4f776809a1fcce8f017972e21e8a9f860dd28bfc
KHartwig <blue.q22@gmail.com>
2015-10-20 10:38:02 -0700

    Added a test for duplicate user and changed combination test to authenticate

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -11,7 +11,7 @@ class UserTest < ActiveSupport::TestCase
   		password: "abc123"
   	)
 
-  	assert_not user.valid_password?("123456")
+  	assert_not user.authenticate("123456")
   end
 
 
@@ -30,6 +30,21 @@ class UserTest < ActiveSupport::TestCase
   		password: "12345"
   	)
 	assert user.invalid?
+  end
+
+  test "disallow duplicate user" do
+  	existing_user = User.create( 
+    				email: "example@gmail.com",
+    				password: "correctpassword",
+    				password_confirmation: "correctpassword",
+    				memberType: "Free")
+    assert existing_user.save, "Did not save valid user"
+    duplicate_user = User.create( 
+    				email: "example@gmail.com",
+    				password: "couldbedifferent",
+    				password_confirmation: "couldbedifferent",
+    				memberType: "Free")
+    assert_not duplicate_user.save, "Created an account with existing username."
   end
 
 end
