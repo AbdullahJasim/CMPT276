adcd71d1f0529baea096e7145516c0e3f384e6cb
Peter Tan <petertan1995@gmail.com>
2015-10-19 22:12:55 -0700

    Added 3 test cases and .gitignore file.

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,32 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+
+    test "incorrect email and pswd combination" do
+  	user = User.create(
+  		email: "example@gmail.com",
+  		password: "abc123"
+  	)
+
+  	assert_not user.valid_password?("123456")
+  end
+
+
+  test "email format invalid" do
+  	user = User.new(
+  		email: "thisIsNotEmail",
+  		password: "abc123"
+  	)
+  	assert user.invalid?
+  end
+
+  
+  test "short password length" do
+  	user = User.new(
+  		email: "example1@gmail.com",
+  		password: "12345"
+  	)
+	assert user.invalid?
+  end
+
 end
