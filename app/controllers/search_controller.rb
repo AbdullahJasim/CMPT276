4f4298a076c18632a8e568756d41d131d4c8a62b
Peter Tan <petertan1995@gmail.com>
2015-10-31 12:57:31 -0700

    Implemented basic search function

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,15 @@
+class SearchController < ApplicationController
+  def new
+    # Keywords are not passed in
+    # Used to forbid users calling GET '/search' directly and
+    # handles the error when user doesn't enter anything in queries
+    if params[:q] == nil || params[:q] == ''
+      redirect_to root_url
+      return
+    else
+      @query = params[:q]
+    end
+
+    @result = $twitter.search(@query, result_type: "recent").take(20)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+
   root 'sessions#new'
 
   #root 'static_pages#home'
@@ -9,6 +10,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
+  get 'search' => 'search#new'
   resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
