c2770cceefdb0bb204ad32dca5fd00437765254b
Peter Tan <petertan1995@gmail.com>
2015-11-11 10:52:17 -0800

    Search controller test cases and bug fix

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -3,7 +3,17 @@ require 'test_helper'
 class SearchControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
-    assert_response :success
+    assert_response 302
+  end
+
+  test "search for ' '(space)" do
+    get :new, { :q => ' ' }
+    assert_redirected_to root_url
+  end
+
+  test "search for (nothing)" do
+    get :new, { :q => '' }
+    assert_redirected_to root_url
   end
 
 end
