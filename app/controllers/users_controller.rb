08ab21670d4a72718c8251cc8344577725b4be38
Mitchell Hole <mhole@sfu.ca>
2015-11-25 21:20:21 -0800

    Users can update their membership type (and not other user's)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,7 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
+
   def new
     if logged_in?
       flash[:danger] = 'Cannot create a new account while logged in'
@@ -24,7 +27,17 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update_attribute(:memberType, params[:user][:memberType])
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
 private
@@ -33,4 +46,19 @@ private
     params.require(:user).permit(:email, :password,
                                  :password_confirmation, :memberType)
   end
+
+  # Confirms a logged-in user.
+  def logged_in_user
+    unless logged_in?
+      flash[:danger] = "Please log in."
+      redirect_to login_url
+    end
+  end
+
+  # Confirms the correct user.
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_url) unless @user == current_user
+  end
+
 end
