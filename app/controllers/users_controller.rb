86f962a29ded46298caa68d94a4772d2e1075883
Mitchell Hole <mhole@sfu.ca>
2015-11-29 16:41:44 -0800

    Admin user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,12 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: [:destroy, :index]
+
+  def index
+    @user = current_user
+    @users = User.all
+  end
 
   def new
     if logged_in?
@@ -12,7 +18,8 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = current_user
+    @user2 = User.find(params[:id])
   end
 
   def create
@@ -28,16 +35,28 @@ class UsersController < ApplicationController
 
   def edit
     @user = current_user
+    @user2 = User.find(params[:id])
   end
 
   def update
-    @user = current_user
-    if @user.update_attribute(:memberType, params[:user][:memberType])
+    if current_user.admin = true
+      @user2 = User.find(params[:id])
+    else
+      @user2 = current_user
+    end
+
+    if @user2.update_attribute(:memberType, params[:user][:memberType])
       flash[:success] = "Profile updated"
-      redirect_to @user
+      redirect_to @user2
     else
       render 'edit'
     end
+  end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
   end
 
 private
@@ -58,7 +77,12 @@ private
   # Confirms the correct user.
   def correct_user
     @user = User.find(params[:id])
-    redirect_to(root_url) unless @user == current_user
+    redirect_to(root_url) unless @user == current_user || current_user.admin?
+  end
+
+  # Confirms an admin user.
+  def admin_user
+    redirect_to(root_url) unless current_user.admin?
   end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create!(
+             email: "admin@sfu.ca",
+             password:              "group13",
+             password_confirmation: "group13",
+             memberType: "Paid",
+             admin: true)
